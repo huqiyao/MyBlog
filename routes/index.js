44830bd6d6f7e4269b59a37d0ce472518a2950a8
AtongSF <AtongSF@gmail.com>
2019-09-08 17:20:43 +0800

    render tags using ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var tags = ["灵魂画手？","五行缺王","日益喜欢霓虹国文化","此人学素描和彩铅","喜欢Macrame编制","想去环游世界","浏览了许多美食","前端爱好者","想写服务人民的好工具","……"];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',
+    tags:tags
+  });
 });
 
 
