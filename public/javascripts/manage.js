e58a2bb0576a37733bd6f1e652b8cac10fe152a5
AtongSF <AtongSF@gmail.com>
2019-09-10 23:38:39 +0800

    modify manager-index

---
+++ public/javascripts/manage.js
@@ -0,0 +1,4 @@
+// function chevron_toggle(){
+//     $("#collapseParent").find("span").toggleClass("glyphicon-chevron-up");
+//     $("#collapseParent").find("span").toggleClass("glyphicon-chevron-down");
+// }
